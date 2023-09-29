63a11dad5429037ed1bc5259c979f254bc56fdda
Aditya <adityanerves@gmail.com>
2023-09-29 11:39:09 +0530

    Day-2 work done

--- forever_care/src/index.js
+++ forever_care/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthContextProvider from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
