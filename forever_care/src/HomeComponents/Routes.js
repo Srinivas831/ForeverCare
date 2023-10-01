bb4665c47b0cff58bf9e775574b9211093b5197b
unknown <anshuldhakate11@gmail.com>
2023-10-01 12:03:06 +0530

    done

--- forever_care/src/HomeComponents/Routes.js
+++ forever_care/src/HomeComponents/Routes.js
@@ -1,26 +1,23 @@
-// routes.js
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 // import { Switch } from '@chakra-ui/react';
 
-import { Home }from '../Pages/Home'; // Create Home component
-// import {Services} from '../serviceComponents/Services'; // Create Services component
-import {About} from '../Pages/About'; // Create About component
-import {Packages} from '../Pages/Packages'; // Create Packages component
-import {Contact} from '../Pages/Contact'; // Create Contact component
-import { SignUp } from '../LoginComponent/SignUp';
+import {About} from '../Pages/About';
+import {Packages} from '../Pages/Packages';
+import {Contact} from '../Pages/Contact';
+import { SignUp } from '../LoginComponent/Signup';
 import { Services } from '../serviceComponents/Services';
 import { SignIn } from '../LoginComponent/SignIn';
 import ServicesData from '../serviceComponents/ServicesData';
+import Home from '../Pages/Home';
 
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/services" component={Services} /> */}
+        <Route path="/" element= { <Home/> } />
         <Route path='/services' element={<Services />}/>
         <Route path="/about" element={<About/>} />
         <Route path="/packages" component={Packages} />
